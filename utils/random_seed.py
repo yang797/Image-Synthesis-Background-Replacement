36a77e618aef9568e07029a8e831780ce88e6058
yang797 <158274306+yang797@users.noreply.github.com>
2025-05-09 18:08:32 +0800

    Add files via upload

---
+++ utils/random_seed.py
@@ -0,0 +1,16 @@
+import random
+import torch
+import os
+import numpy as np
+
+
+# Set random seed for reproducibility
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
